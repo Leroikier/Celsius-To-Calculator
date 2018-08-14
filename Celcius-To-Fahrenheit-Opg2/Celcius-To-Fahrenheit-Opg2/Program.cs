4403e289264e11507de73ec444239cb261ab2db4
Alexander Roikier Møller <alex6308@elev.tec.dk>
2018-08-14 13:16:33 +0200

    <Start på opgaven>

---
+++ Celcius-To-Fahrenheit-Opg2/Celcius-To-Fahrenheit-Opg2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Celcius_To_Fahrenheit_Opg2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("This is a Celcius to Fahrenheit, Kelvin, Réaumur Calculator");
+            Console.WriteLine("");
+            Console.Write("Put in the degrees in Fahrenheit ");
+            fahrenheit = Convert.ToDouble(Console.ReadLine());
+
+
+            Console.ReadKey();
+        }
+    }
+}
